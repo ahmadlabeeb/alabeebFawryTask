0c0eb86f9e452c278629b8c490dcab60a4894618
Ahmad Labeeb <alabeeb81@gmail.com>
2021-11-29 11:04:49 +0200

    details scene update with rx

--- Fawry-Task/Scenes/Details/DetailsViewController.swift
+++ Fawry-Task/Scenes/Details/DetailsViewController.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class DetailsViewController: UIViewController {
     
     @IBOutlet weak var authorLabel: UILabel!
     @IBOutlet weak var authorImage: UIImageView!
-
+    private let disposeBag = DisposeBag()
     var viewModel: DetailsViewModel?
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +22,18 @@ class DetailsViewController: UIViewController {
     }
     func setupUI() {
         authorLabel.text = viewModel?.item.author
-        authorImage.fromUrlString(urlString: viewModel?.item.downloadURL ?? "") {[weak self] in
-            self?.view.backgroundColor = self?.authorImage.image?.averageColor
-        }
-        self.view.backgroundColor = self.authorImage.image?.averageColor
-        authorLabel.textColor = self.view.backgroundColor?.inverted
+        authorImage.fromUrlString(urlString: viewModel?.item.downloadURL ?? "")
+        authorImage.rx.observe(UIImage.self, "image")
+            .subscribe(onNext: { [weak self] image in
+                if let image = image {
+                    self?.view.backgroundColor = image.averageColor
+                    self?.authorLabel.textColor = self?.view.backgroundColor?.inverted
+                }else{
+                    self?.view.backgroundColor = .white
+                    self?.authorLabel.textColor = .black
+                }
+            })
+            .disposed(by: disposeBag)
     }
 
 }
